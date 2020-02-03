aa4b5ff53ebdb6edb831d9a92fe87c9d8f004fb9
Max Rosenthal <mxrosenthal@yahoo.com>
2020-02-03 22:50:30 +0000

    completed join queries.

---
+++ 2_queries_joins/2_cohortsTotalAssignmentDuration.sql
@@ -0,0 +1,4 @@
+SELECT SUM(assignment_submissions.duration) AS total_duration
+FROM students JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
