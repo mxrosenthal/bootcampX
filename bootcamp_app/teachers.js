b17f6b1dbc64d9ed0e6d2b5211fbe7f5dc0295b6
Max Rosenthal <mxrosenthal@yahoo.com>
2020-02-05 21:51:17 +0000

    conect postgresSQL database from a JS app using the node-postgres library. can connect to any postgres database from any node app.

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 
 const args = process.argv.slice(2);
 let cohortQueried = args[0];
-let listLength = args[1];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -14,19 +13,18 @@ const pool = new Pool({
 pool
   .query(
     `
-    SELECT students.id AS id, students.name AS name, cohorts.name AS cohort
-    FROM students
-    JOIN cohorts
-    ON cohort_id = cohorts.id
+        SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort
+    FROM assistance_requests
+    JOIN teachers ON teacher_id = teachers.id
+    JOIN students ON student_id = students.id
+    JOIN cohorts ON cohort_id = cohorts.id
     WHERE cohorts.name LIKE '%${cohortQueried}%'
-    LIMIT ${listLength || 5};
-    `
+    ORDER BY teacher;
+`
   )
   .then(res => {
     res.rows.forEach(user => {
-      console.log(
-        `${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`
-      );
+      console.log(`${user.cohort}: ${user.teacher}`);
     });
   })
   .catch(err => console.error('query error', err.stack));
